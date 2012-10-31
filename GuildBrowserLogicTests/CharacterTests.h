a13a1d62527848e31167cf78f01c6ff9173549ea
WayLoon Tan <wayloontan@gmail.com>
2012-10-31 11:50:39 -0500

    Added a couple more tests

---
+++ GuildBrowserLogicTests/CharacterTests.h
@@ -0,0 +1,13 @@
+//
+//  CharacterTests.h
+//  GuildBrowser
+//
+//  Created by Wayloon Tan on 10/31/12.
+//  Copyright (c) 2012 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CharacterTests : SenTestCase
+
+@end
