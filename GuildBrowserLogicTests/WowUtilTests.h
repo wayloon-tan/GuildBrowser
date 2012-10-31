d6b444199150b42e98392846cae989c35a5216ad
WayLoon Tan <wayloontan@gmail.com>
2012-10-31 11:26:37 -0500

    Added initial test cases

---
+++ GuildBrowserLogicTests/WowUtilTests.h
@@ -0,0 +1,13 @@
+//
+//  WowUtilTests.h
+//  GuildBrowser
+//
+//  Created by Wayloon Tan on 10/31/12.
+//  Copyright (c) 2012 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WowUtilTests : SenTestCase
+
+@end
